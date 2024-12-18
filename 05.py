f67cd72f8d047563946870ad3415de14d18afd76
BraSteAU <brayan_8924@hotmail.com>
2024-12-17 22:16:58 -0500

    ejercicio5
    Ejercicio 5 resuelto

--- 05.py
+++ 05.py
@@ -1,3 +1,6 @@
+#Escribe un programa que solicite al usuario un número entero positivo y luego imprima todos los 
+# números primos menores o iguales a ese número utilizando un bucle while.
+
 numero=int(input("Ingresa un numero: "))
 i=2
 while i<numero:
